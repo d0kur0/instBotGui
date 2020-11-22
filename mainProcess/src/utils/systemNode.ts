4b78784e69da5c618c4e5850ecf05360446250c3
d0kur0 <dev.dokuro@gmail.com>
2020-11-22 15:26:22 +0300

    Add await for exec

--- mainProcess/src/utils/systemNode.ts
+++ mainProcess/src/utils/systemNode.ts
@@ -2,13 +2,13 @@ import { exec } from "promisify-child-process";
 
 export const isNodeInstalled = async (): Promise<boolean> => {
   return (
-    exec("node -v").then(
+    (await exec("node -v").then(
       () => true,
-      () => false,
-    ) &&
-    exec("npm -v").then(
+      () => false
+    )) &&
+    (await exec("npm -v").then(
       () => true,
-      () => false,
-    )
+      () => false
+    ))
   );
 };
